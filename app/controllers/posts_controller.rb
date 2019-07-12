0e8ad77f2765afdcd5a1ec75b74e7ef4c7201a3f
Nataliya R <n.rudenko@gmail.com>
2019-07-12 19:51:02 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,8 +28,8 @@ class PostsController < ApplicationController
 	end
 	private
  
-def post_params
-  params.require(:post).permit(:title, :description)
+def post_params(*args)
+  params.require(:post).permit(*args)
 end
 
 end
